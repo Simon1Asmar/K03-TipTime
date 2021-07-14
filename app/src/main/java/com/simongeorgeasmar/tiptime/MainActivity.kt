a2f64c9a9787fbdc6f99b8d374175709b5d97f78
Simon1GA <72914663+Simon1GA@users.noreply.github.com>
2021-07-14 14:48:09 +0300

    Version 0.8 made some functions private, inlined some parts of the code and got rid of repetitive code to adopt better coding practices

--- app/src/main/java/com/simongeorgeasmar/tiptime/MainActivity.kt
+++ app/src/main/java/com/simongeorgeasmar/tiptime/MainActivity.kt
@@ -7,7 +7,7 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,20 +17,17 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener { calculateTip() }
     }
 
-    fun calculateTip() {
+    private fun calculateTip() {
 
         val stringInTextField = binding.costOfService.text.toString()
        //turned it to toDoubleOrNull() instead of just toDouble() so it doesn't crash if there is no input
         val cost = stringInTextField.toDoubleOrNull()
         if(cost == null){
-            //This will clear the tipResult
-            binding.tipResult.text = ""
+            displayTip(0.0)
             return
         }
 
-        val selectedID = binding.tipOptions.checkedRadioButtonId
-
-        val tipPercentage = when (selectedID) {
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -38,14 +35,16 @@ class MainActivity : AppCompatActivity() {
 
         var tip = tipPercentage * cost
 
-        val roundUp = binding.roundUpSwitch.isChecked
-
-        if (roundUp) {
+        if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
+
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip : Double){
         //this formats the tip into the way the currency of the user is usually formatted because different currencies have different formats
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 
